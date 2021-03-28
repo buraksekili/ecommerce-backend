b210c6b4debc5ec3c3535ba0ea27876a8fac65ad
Nureddin Kamadan <nkamadan@sabanciuniv.edu>
2021-03-28 09:02:18 +0300

    Initialization of login feature

---
+++ src/routes/login.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("../db/models/User");
+
+const loginRouter = express.Router();
+
+loginRouter.get("/", (req, res) => res.send("In Login"));
+
+loginRouter.post("/api/login", async (req, res) => {
+  const { userEmail, password, username } = req.body;
+
+  if (!validateReqBody(userEmail, password, username)) {
+    return res
+      .status(401)
+      .send({ status: false, type: "INVALID", error: "invalid request body" });
+  }
+
+  try {
+    const newUser = new User({
+      email: userEmail,
+      password: password,
+      username: username,
+    });
+    await newUser.save();
+  } catch (error) {
+    const validationErr = getErrors(error);
+    return res
+      .status(401)
+      .send({ status: false, type: "VALIDATION", error: validationErr });
+  }
+
+  res.send({ status: true });
+});
+
+// Validates request body
+const validateReqBody = (...req) => {
+  for (r of req) {
+    if (!r || r.trim().length == 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+// Checks errors returning from DB
+const getErrors = (error) => {
+  if (error instanceof mongoose.Error.ValidationError) {
+    let validationErr = "";
+    for (field in error.errors) {
+      validationErr += `${field} `;
+    }
+    return validationErr.substring(0, validationErr.length - 1);
+  }
+  return error.message;
+};
+
+module.exports = { signupRouter };
