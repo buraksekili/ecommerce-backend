c5668fb3c1fab713cf9a0f053594e66b4eceee99
s21buraksekili <buraksekili@sabanciuniv.edu>
2021-04-01 20:53:21 +0300

    added POSTS request for product

--- src/db/models/Product.js
+++ src/db/models/Product.js
@@ -1,38 +1,42 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const ProductSchema = new mongoose.Schema({
-  productName: {
-    type: String,
-    required: [true, "Product Name is required"],
-    trim: true,
+const ProductSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: [true, "Product Name is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product Description is required"],
+      trim: true,
+    },
+    unitPrice: {
+      type: Number,
+      required: [true, "Unit Price is required"],
+    },
+    categoryID: {
+      type: Number,
+      required: [true, "Category ID is required"],
+    },
+    rate: {
+      type: Number,
+      required: [true, "Rate is required"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Stock is required"],
+    },
+    warranty: {
+      type: Number,
+      required: [true, "Warranty is required"],
+    },
   },
-  description: {
-    type: String,
-    required: [true, "Product Description is required"],
-    trim: true,
-  },
-  unitPrice: {
-    type: Number,
-    required: [true, "Unit Price is required"],
-  },
-  categoryID: {
-    type: Number,
-    required: [true, "Category ID is required"],
-  },
-  rate: {
-    type: Number,
-    required: [true, "Rate is required"],
-  },
-  stock: {
-    type: Number,
-    required: [true, "Stock is required"],
-  },
-  warranty: {
-    type: Number,
-    required: [true, "Warranty is required"],
-  },
-});
+  { versionKey: false }
+);
+
 
 const Product = mongoose.model("Product", ProductSchema);
 module.exports = { Product };
--- src/routes/product.js
+++ src/routes/product.js
@@ -4,6 +4,7 @@ const { validateReqBody, getErrors } = require("../helpers");
 
 const productRouter = express.Router();
 
+// Create Product
 productRouter.post("/api/product", async (req, res) => {
   const {
     productName,
@@ -49,6 +50,100 @@ productRouter.post("/api/product", async (req, res) => {
   }
 
   res.send({ status: true });
+});
+
+// UPDATE product. Takes request body
+productRouter.post("/api/update/product", async (req, res) => {
+  const {
+    _id,
+    productName,
+    description,
+    unitPrice,
+    categoryID,
+    stock,
+    warranty,
+    rate,
+  } = req.body;
+
+  if (
+    !validateReqBody(
+      _id,
+      productName,
+      description,
+      unitPrice,
+      categoryID,
+      stock,
+      warranty,
+      rate
+    )
+  ) {
+    return res
+      .status(401)
+      .send({ status: false, type: "INVALID", error: "invalid request body" });
+  }
+  try {
+    const newProduct = new Product({
+      _id,
+      productName,
+      description,
+      unitPrice,
+      categoryID,
+      stock,
+      warranty,
+      rate,
+    });
+    const product = await Product.findByIdAndUpdate(_id, { $set: newProduct });
+    if (!product) {
+      return res.status(404).send({ status: false, _id });
+    }
+    return res.status(201).send({ status: true, product });
+  } catch (error) {
+    const validationErr = getErrors(error);
+    console.log(validationErr);
+    return res
+      .status(401)
+      .send({ status: false, type: "VALIDATION", error: validationErr });
+  }
+});
+
+// DELETE products.
+productRouter.delete("/api/product/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(401).send({
+      status: false,
+      type: "INVALID",
+      error: "Invalid request parameter, id",
+    });
+  }
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).send({ status: false, id });
+    }
+    return res.status(201).send({ status: true, id });
+  } catch (error) {
+    const validationErr = getErrors(error);
+    console.log(validationErr);
+    return res
+      .status(401)
+      .send({ status: false, type: "VALIDATION", error: validationErr });
+  }
+});
+
+// GET all products
+productRouter.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    return res.send({ status: true, products });
+  } catch (error) {
+    const validationErr = getErrors(error);
+    console.log(validationErr);
+    return res
+      .status(401)
+      .send({ status: false, type: "VALIDATION", error: validationErr });
+  }
 });
 
 module.exports = { productRouter };
