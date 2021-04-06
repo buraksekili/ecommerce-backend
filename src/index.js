c8b9d83b6d8728954f43a421dc3032649b10694a
s21buraksekili <buraksekili@sabanciuniv.edu>
2021-04-06 22:46:29 +0300

    Add DB connector for MongoDB Cluster

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const {
   userRouter,
 } = require("./routes");
 const { connectDB } = require("./db");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,7 +21,13 @@ app.use("/", productRouter);
 app.use("/admin", userRouter);
 
 if (process.env.NODE_ENV !== "test") {
-  connectDB();
+  const MONGO_URI = `mongodb+srv://${
+    process.env.MONGO_USERNAME
+  }:${encodeURIComponent(
+    process.env.MONGO_PASSWORD
+  )}@cluster0.kyaly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+  connectDB(MONGO_URI);
   app.listen(port, () => console.log("Server is running on", port));
 }
 
