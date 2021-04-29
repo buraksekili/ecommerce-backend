4605027d9956dc827460a19d6d05e69026b5403c
Burak Sekili <buraksekili@sabanciuniv.edu>
2021-04-29 17:55:01 +0300

    Add test for product and comment

--- test/user.test.js
+++ test/user.test.js
@@ -4,7 +4,7 @@ const { User } = require("../src/db");
 const mongoose = require("mongoose");
 const { MONGO_OPTIONS } = require("../src/config");
 const { dropAllCollections, removeAllCollections } = require("./helpers");
-jest.setTimeout(50000);
+jest.setTimeout(60000);
 
 const MONGO_TEST_URI = `mongodb://admin2:${encodeURIComponent(
   "example"
@@ -35,11 +35,6 @@ beforeEach(async () => {
     await newUser.save();
     testUser["_id"] = newUser._id;
     testUser["token"] = newUser.token;
-    // const newUser = new User(testUser);
-    // const token = await newUser.getJWT();
-    // testUser["token"] = token;
-    // const user = await newUser.save();
-    // testUser["_id"] = user._id;
   } catch (error) {
     console.error("error in creating user", error);
   }
@@ -59,8 +54,6 @@ describe("GET /admin user", () => {
     try {
       const result = await request(app).get(url);
 
-      console.log("result", result.body);
-
       // status check
       expect(result.status).toEqual(200);
       expect(result.body.status).toEqual(true);
@@ -73,6 +66,111 @@ describe("GET /admin user", () => {
 
       const { userEmail, username } = testUser;
       expect(result.body.users[0]).toMatchObject({ userEmail, username });
+
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+
+  it("should get all comments", async (done) => {
+    const url = "/api/comments";
+    try {
+      const result = await request(app).get(url);
+
+      // status check
+      expect(result.status).toEqual(200);
+      expect(result.body.status).toEqual(true);
+      expect(result.body.comments).toEqual([]);
+
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+
+  it("should add comment ", async (done) => {
+    let url = "/api/product";
+    try {
+      const testProduct = {
+        productName: "test",
+        description: "test",
+        unitPrice: 20,
+        categoryID: 3,
+        stock: 20,
+        warranty: 1,
+        userType: 2,
+        imagePath: "test",
+      };
+
+      let result = await request(app).post(url).send(testProduct);
+
+      const product = result.body.product;
+      url = "/api/comment";
+      const c = {
+        productID: product._id,
+        content: "comment",
+        token: testUser.token,
+      };
+
+      result = await request(app).post(url).send(c);
+      // status check
+      expect(result.status).toEqual(200);
+      expect(result.body.status).toEqual(true);
+      expect(result.body.comments[0].owner).toEqual(testUser.username);
+      expect(result.body.comments[0].content).toEqual(c.content);
+
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+
+  it("should get all products", async (done) => {
+    const url = "/api/products";
+    try {
+      const result = await request(app).get(url);
+
+      // status check
+      expect(result.status).toEqual(200);
+      expect(result.body.status).toEqual(true);
+      expect(result.body.products).toEqual([]);
+
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+
+  it("should get all products2", async (done) => {
+    const url = "/api/product";
+    try {
+      const testProduct = {
+        productName: "test",
+        description: "test",
+        unitPrice: 20,
+        categoryID: 3,
+        stock: 20,
+        warranty: 1,
+        userType: 2,
+        imagePath: "test",
+      };
+
+      const result = await request(app).post(url).send(testProduct);
+      const resultProduct = result.body.product;
+      delete resultProduct._id;
+      delete resultProduct.comments;
+      delete resultProduct.previousPrice;
+      delete resultProduct.rate;
+      delete resultProduct.rateCount;
+      delete resultProduct.rateTotal;
+
+      delete testProduct.userType;
+
+      // status check
+      expect(result.status).toEqual(200);
+      expect(result.body.status).toEqual(true);
+      expect(result.body.product).toEqual(testProduct);
 
       done();
     } catch (error) {
